d62c5ac5c2cf268c3c5b8447c8c861bf59e58527
FRED MASHAL <32965534+CeoFred@users.noreply.github.com>
2018-03-11 17:48:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>WorkIT SoftWare Company Limited</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <!-- Material Design Bootstrap -->
-    <link href="css/mdb.min.css" rel="stylesheet">
-    <!-- Your custom styles (optional) -->
-    <link href="css/style.css" rel="stylesheet">
-
-<link rel="shortcut icon" type="image/jpg" href="img/logo1.png">
-</head>
+    <?php include'inc/header.php'; ?>
+    </head>
 <body style="">
 <!--Main Navigation-->
 <header>
