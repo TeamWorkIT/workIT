b62acb69d3bb58d5a191ba20f48a1b8d06ce73f8
FRED MASHAL <32965534+CeoFred@users.noreply.github.com>
2018-03-11 16:42:13 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,317 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>WorkIT SoftWare Company Limited</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="css/style.css" rel="stylesheet">
+
+<link rel="shortcut icon" type="image/jpg" href="img/logo1.png">
+</head>
+<body style="">
+<!--Main Navigation-->
+<header>
+<?php include'inc/nav.php'; ?>        
+</header>
+<!--Main Navigation-->
+<!--Main layout-->
+<main>
+<!--Main container-->
+<div class="container" style="margin-top: 70px;">
+    <!--Grid row-->
+    <div class="row justify-content-md-center">
+        <!--Grid column-->
+        <div class="col-md-7">
+<div class="view overlay hm-white-light z-depth-1-half">
+    <img src="img/main.jpg" class="img-fluid " alt="">
+    <div class="mask"></div>
+</div>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-md-5">
+
+    
+    <h1></h1>
+    <hr>
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis pariatur quod ipsum atque quam dolorem
+        voluptate officia sunt placeat consectetur alias fugit cum praesentium ratione sint mollitia, perferendis
+        natus quaerat!</p>
+    <a href="!#" class="btn btn-primary">Call</a><a class="btn btn-outline-success btn-rounded waves-effect" href="">Email</a><a class="btn btn-outline-warning btn-rounded waves-effect" href="">Chat</a>
+
+
+
+        </div>
+        <!--Grid column-->
+
+    </div>
+    <!--Grid row-->
+
+
+<hr>
+<h1 style="text-align: center;">Our-Services</h1>
+<hr>
+<!--Grid row-->
+<div class="row">
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-4">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img style="width: 100%;height: 190px;" src="img/ecom.png" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+                <!--Title-->
+                <h4 class="card-title">E-Commerce Websites</i></h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-4">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img src="img/digima.jpg" style="height: 190px;width: 100%;" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body" id="Digital">
+                <!--Title-->
+                <h4 class="card-title">Digital Marketing</h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-outline-success">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-4">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img src="img/seo.jpg" style="height: 190px;width: 100%" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+                <!--Title-->
+                <h4 class="card-title">Search Engine Optimization</h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-outline-success">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+</div>
+<!--Grid row-->
+
+
+<hr>
+<!--Grid row-->
+<div class="row">
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-12">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img style="width: 100%;height: 190px;" src="img/ecom.png" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+                <!--Title-->
+                <h4 class="card-title">E-Commerce Websites</i></h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-outline-success">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-6">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img src="img/graph.png" style="height: 190px;width:100%" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+                <!--Title-->
+                <h4 class="card-title">Digital Marketing</h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-outline-success">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+    <!--Grid column-->
+    <div class="col-lg-4 col-md-6">
+
+        <!--Card-->
+        <div class="card">
+
+            <!--Card image-->
+            <div class="view overlay hm-white-slight">
+                <img src="img/wp.png" style="height: 190px;width: 100%" class="img-fluid" alt="">
+                <a href="#">
+                    <div class="mask"></div>
+                </a>
+            </div>
+
+            <!--Card content-->
+            <div class="card-body">
+                <!--Title-->
+                <h4 class="card-title">Wordpress Themes</h4>
+                <!--Text-->
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-outline-success">Button</a>
+            </div>
+
+        </div>
+        <!--/.Card-->
+
+    </div>
+    <!--Grid column-->
+
+</div>
+<!--Grid row-->
+                                          
+</div>
+<!--Main container-->
+
+
+
+</main>
+<!--Main layout-->
+
+
+<!--Footer-->
+<footer class="page-footer grey center-on-small-only">
+
+    <!--Footer Links-->
+    <div class="container-fluid">
+        <div class="row">
+
+            <!--First column-->
+            <div class="col-md-6">
+                <h5 class="title">Footer Content</h5>
+                <p>Here you can use rows and columns here to organize your footer content.</p>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-6">
+                <h5 class="title">Links</h5>
+                <ul>
+                    <li><a href="#!">Link 1</a></li>
+                    <li><a href="#!">Link 2</a></li>
+                    <li><a href="#!">Link 3</a></li>
+                    <li><a href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <!--/.Second column-->
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!--Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2018 Copyright: <a href="#">FastFingaz </a>
+
+        </div>
+    </div>
+    <!--/.Copyright-->
+
+</footer>
+<!--/.Footer-->
+                                      
+
+
+
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
+
