b61dd49891283a06b1dc36bfd2d68a8833849a63
FRED MASHAL <32965534+CeoFred@users.noreply.github.com>
2018-03-11 17:55:01 +0100

    Add files via upload

---
+++ user/profile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>User Profile| GodzFingers MDB</title>
+<?php include 'inc/header.php'; ?>
+</head>
+<body>
+<!--Main Navigation-->
+<header>
+<?php include'inc/nav.php'; ?>        
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main>
+<!--Main container-->
+</main>
+</body>
+</html>
