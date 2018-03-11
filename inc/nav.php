1fa00cf1cf0798681de50b42056fedc9896c05d6
FRED MASHAL <32965534+CeoFred@users.noreply.github.com>
2018-03-11 17:51:00 +0100

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,51 @@
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color:#2BBBAD">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="index.php"><img src="img/logo1.png" class="img responsive img-fuid" height="50"></a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="webapps.php">WebApplications</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="graphicsdesign.php">GraphicsDesign</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="ecommerceapps.php">ModernE-commerce</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="wordPressapps.php">WordPressCMSApps</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="andriodapps.php">AndriodApps</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="iosapps.php">IosApps</a>
+            </li>
+
+    </ul>
+        <!-- Links -->
+
+        <!-- Search form -->
+
+        <span><i class="fa fa-shopping-cart" style="font-size: 26px;"></i></span>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2"  type="text" placeholder="Search" aria-label="Search">
+        </form>
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->               
