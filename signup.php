e1c1337bf7702825ef8e83507ed55c120d62fee4
FRED MASHAL <32965534+CeoFred@users.noreply.github.com>
2018-03-19 12:18:58 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login |WorkIT SoftWare Company Limited</title>
+    <?php include'inc/header.php'; ?>
+    </head>
+<body style="background-image: url(img/backg.jpg);background-size: cover;">
+<!--Main Navigation-->
+<header>
+<?php include'inc/nav.php'; 
+
+?>        
+</header>
+<!--Main Navigation-->
+<!--Main layout-->
+<main style="margin-top: 100px;text-align: center;width: 360px;margin-left: 500px;opacity: 0.8">
+	
+	<div class="form form-control" >
+<h2>Signup</h2>
+	<form method="POST" action="inc/signup.inc.php">
+		<input type="text" required="" name="first" class="input form-control"  placeholder="First Name">
+		<br>
+
+		<input type="text" required="" name="last" class="input form-control"  placeholder="Last Name">
+		<br>
+
+		<input type="text" required="" name="username" class="input form-control"  placeholder="Choose a Username">
+		<br>
+
+		<input type="text" required="" name="email" class="input form-control"  placeholder="Email">
+		<br>
+
+		<input type="text" name="pwd" class="form-control" required="" placeholder="Password">
+		<button name="submit" type="submit" class="btn btn-success">Sign Up</button>
+		Already a user? <a href="Login.php">Login</a>
+	</form>
+</div>
+</main>
+
+</body>
+</html>
